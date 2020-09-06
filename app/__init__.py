58adbb23bd3eee6241ee8837aad84c2eb26373ee
Iaroslav <66280163+Iaroslavv@users.noreply.github.com>
2020-09-07 01:33:32 +0300

    Feature/create navbar (#2)
    * Add requirements
    
    * remove config from the file
    
    * Add static folder
    
    * Add navbar

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.static_folder = 'static'
+app.config.from_object("app.config.config")
+
 from app import routes
--- run.py
+++ run.py
@@ -1,7 +1,4 @@
 from app import app
 
-app.config.from_object("app.config.config")
-
-
 if __name__ == "__main__":
     app.run()
