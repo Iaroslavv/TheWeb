b3335fddce028327cf6862fc93cfac68e958259b
Iaroslav <66280163+Iaroslavv@users.noreply.github.com>
2020-10-17 22:31:15 +0300

    Bag/fix navbar (#4)
    * Half fix navbar
    
    * Half fix navbar attempt two
    
    * Attempt three with navbar
    
    * Half fix 4
    
    * Add js file
    
    * change main page route
    
    * Add sticky nav
    
    * Add logo
    
    * Update app/templates/layout.html

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template
 
 
-@app.route("/about")
+@app.route("/")
 def index():
     mytitle = "Heyheyehey"
     return render_template("layout.html", mytitle=mytitle)
